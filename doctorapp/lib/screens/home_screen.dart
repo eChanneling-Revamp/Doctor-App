652dbc1571842f73d3bd157ffb2f5526525d722e
sakith <sakiththewmika71@gmail.com>
2025-10-07 11:42:15 +0530

    Add bottom navigation bar to HomeScreen
    Introduced a reusable HomeBottomNavigationBar widget and integrated it into HomeScreen to allow navigation between different sections. The navigation bar supports five items: Home, Appointment, Session, Teleconsult, and Income.

--- doctorapp/lib/screens/home_screen.dart
+++ doctorapp/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/home_widgets/appbar_widgets.dart';
+import '../widgets/home_widgets/navigation_bar_widgets.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,12 +10,21 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: CustomAppBar(),
+      bottomNavigationBar: HomeBottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
     );
   }
 }
---
+++ doctorapp/lib/widgets/home_widgets/navigation_bar_widgets.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HomeBottomNavigationBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const HomeBottomNavigationBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: currentIndex,
+      onTap: onTap,
+      selectedItemColor: const Color(0xFF4A3FFF),
+      unselectedItemColor: Colors.grey.shade600,
+      backgroundColor: Colors.white,
+      elevation: 8,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_today),
+          label: 'Appointment',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.access_time),
+          label: 'Session',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.video_call),
+          label: 'Teleconsult',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.attach_money),
+          label: 'Income',
+        ),
+      ],
+    );
+  }
+}
